7ec882cc75f0f22bd45d39fa9a4e066f0e3f8f0f
Francisco <Francisco>
2024-04-23 17:36:13 +0200

    Move Writer example

--- ExcelProcessor.Example/Writer/DataContext/WriterDataContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExcelProcessor.Example.Writer.DataContext
-{
-    internal class WriterDataContext
-    {
-        public string Title { get; set; }
-        public string SubTitle { get; set; }
-        public IEnumerable<UserInfo> Users { get; set; }
-    }
-}
