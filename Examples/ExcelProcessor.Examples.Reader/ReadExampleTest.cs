cb292ca30002d036540ada652b8506255ccafcce
Francisco <Francisco>
2024-04-29 12:08:00 +0200

    ReadWithErrors

--- Examples/ExcelProcessor.Examples.Reader/ReadExampleTest.cs
+++ Examples/ExcelProcessor.Examples.Reader/ReadExampleTest.cs
@@ -14,7 +14,7 @@ namespace ExcelProcessor.Examples.Reader
         public void ReadOkTest()
         {
             IExcelGenerator excelGenerator = new ExcelGenerator();
-            using (IExcelReaderEngine readerEngine = excelGenerator.ReadFromFile("Resources\\ReaderExample.xlsx"))
+            using (IExcelReaderEngine readerEngine = excelGenerator.ReadFromFile("Resources\\ReaderExampleOk.xlsx"))
             {
                 IExcelReaderResult<StudentContext> result = readerEngine.ReadFile(new IExcelSheetParser<StudentContext>[]
                     {
@@ -34,6 +34,46 @@ namespace ExcelProcessor.Examples.Reader
                 ValidateStudent(result.EntityReaded.Students.ElementAt(0), "Sebastian", "Gil", 22);
                 ValidateStudent(result.EntityReaded.Students.ElementAt(1), "Pedro Jose", "Castro", 25);
                 ValidateStudent(result.EntityReaded.Students.ElementAt(2), "Inmaculada", "Amat", 21);
+                ValidateStudent(result.EntityReaded.Students.ElementAt(3), "Margarita", "Palma", 33);
+                ValidateStudent(result.EntityReaded.Students.ElementAt(4), "Isidoro", "Lobo", 34);
+                ValidateStudent(result.EntityReaded.Students.ElementAt(5), "Marcela", "Prado", 29);
+            }
+        }
+
+        [Fact]
+        public void ReadWithErrorsTest()
+        {
+            IExcelGenerator excelGenerator = new ExcelGenerator();
+            using (IExcelReaderEngine readerEngine = excelGenerator.ReadFromFile("Resources\\ReaderExampleErrors.xlsx"))
+            {
+                IExcelReaderResult<StudentContext> result = readerEngine.ReadFile(new IExcelSheetParser<StudentContext>[]
+                    {
+                        new ExcelSheetParserExample()
+                    });
+
+                Assert.NotNull(result);
+
+                // Errors detected
+                Assert.NotEmpty(result.Errors);
+                Assert.Equal(3, result.Errors.Count());
+
+                // Errors
+                Assert.Equal("B4", result.Errors.ElementAt(0).Cell);
+                Assert.Equal("Generated at is not a valid date", result.Errors.ElementAt(0).ErrorDescription);
+
+                Assert.Equal(8, result.Errors.ElementAt(1).RowNumError);
+                Assert.Equal("Last name is mandatory", result.Errors.ElementAt(1).ErrorDescription);
+
+                Assert.Equal(10, result.Errors.ElementAt(2).RowNumError);
+                Assert.Equal("Age is mandatory and must be a number", result.Errors.ElementAt(2).ErrorDescription);
+
+                // Data ok
+                Assert.NotNull(result.EntityReaded);
+                Assert.Equal("MIT", result.EntityReaded.University);
+                Assert.Equal(6, result.EntityReaded.Students.Count);
+                ValidateStudent(result.EntityReaded.Students.ElementAt(0), "Sebastian", null, 22);
+                ValidateStudent(result.EntityReaded.Students.ElementAt(1), "Pedro Jose", "Castro", 25);
+                ValidateStudent(result.EntityReaded.Students.ElementAt(2), "Inmaculada", "Amat", 0);
                 ValidateStudent(result.EntityReaded.Students.ElementAt(3), "Margarita", "Palma", 33);
                 ValidateStudent(result.EntityReaded.Students.ElementAt(4), "Isidoro", "Lobo", 34);
                 ValidateStudent(result.EntityReaded.Students.ElementAt(5), "Marcela", "Prado", 29);
